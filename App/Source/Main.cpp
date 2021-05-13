1f6054e8ded37243dfaabf40f0bea650ea020187
Clymiaru <lscirno@gmail.com>
2021-05-14 01:28:42 +0800

    Reimported SFML, created basic Console Log utils, and tested OpenGL integration

--- App/Source/Main.cpp
+++ App/Source/Main.cpp
@@ -1,6 +1,43 @@
 #include "pch.h"
 
+#include <glad.h>
+#include <SFML/Graphics.hpp>
+
+#include "Utils/Log.h"
+
 int main()
 {
-	std::cout << "Testing\n";
+	sf::Window window(sf::VideoMode(1280, 720),
+		"SFML OpenGL(glad) Integration!",
+		sf::Style::Default,
+		sf::ContextSettings(24));
+	window.setVerticalSyncEnabled(true);
+
+	LOG("Testing logging!");
+	ASSERT(true, "Window cannot be set to active!");
+	
+	gladLoadGL();
+	
+	bool isRunning = true;
+	while (isRunning)
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				isRunning = false;
+			}
+			else if (event.type == sf::Event::Resized)
+			{
+				glViewport(0, 0, event.size.width, event.size.height);
+			}
+		}
+
+		// Draw
+		glClearColor(1.0f, 0.0f, 0.0f ,1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+		window.display();
+	}
 }
--- premake5.lua
+++ premake5.lua
@@ -22,6 +22,10 @@ OutputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Libraries --
 Libraries = {}
+Libraries["SFML"] = "%{GameName}/Library/SFML"
+Libraries["glad"] = "%{GameName}/Library/glad"
+
+include "App/Library/glad"
 
 project (GameName)
 	location (GameName)
@@ -43,29 +47,69 @@ project (GameName)
 
 	includedirs {
 		"%{prj.name}/Source",
-	}
-
-	libdirs {
+		"%{Libraries.glad}",
+		"%{Libraries.SFML}/include",
 	}
 
 	defines {
 		"_CRT_SECURE_NO_WARNINGS",
+		"SFML_STATIC"
 	}
+
+	filter {"platforms:Win64", "configurations:Debug"}
+		libdirs {
+			"%{Libraries.SFML}/lib"
+	    }
+
+    postbuildcommands {
+        ("{COPY} %{prj.location}Library/SFML/bin/openal32.dll %{wks.location}Binaries/"..OutputDir.. "/%{GameName}")
+    }
+
 
 	filter "configurations:Debug"
 		defines "DEBUG"
-		buildoptions "/MTd"
+		buildoptions "/MDd"
 		runtime "Debug"
 		symbols "on"
 
 		links {
+			"glad",
+			"opengl32.lib",
+            "freetype.lib",
+            "winmm.lib",
+            "gdi32.lib",
+            "openal32.lib",
+            "flac.lib",
+            "vorbisenc.lib",
+            "vorbisfile.lib",
+            "vorbis.lib",
+            "ogg.lib",
+            "sfml-audio-s-d.lib",
+            "sfml-graphics-s-d.lib",
+            "sfml-window-s-d.lib",
+            "sfml-system-s-d.lib"
 		}
 
 	filter "configurations:Release"
 		defines "NDEBUG"
-		buildoptions "/MT"
+		buildoptions "/MD"
 		runtime "Release"
 		optimize "on"
 
 		links {
+			"glad",
+			"opengl32.lib",
+            "freetype.lib",
+            "winmm.lib",
+            "gdi32.lib",
+            "openal32.lib",
+            "flac.lib",
+            "vorbisenc.lib",
+            "vorbisfile.lib",
+            "vorbis.lib",
+            "ogg.lib",
+            "sfml-audio-s.lib",
+            "sfml-graphics-s.lib",
+            "sfml-window-s.lib",
+            "sfml-system-s.lib"
 		}
